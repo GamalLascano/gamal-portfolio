30362aa6c8a684947bc843e65c7edafbfe2d80af
GamalLascano <gamallascano@gmail.com>
2023-04-05 17:19:29 -0300

    Using next fonts

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import { Raleway } from "@next/font/google";
+const raleway = Raleway({
+  weight: "600",
+  style: "normal",
+});
 function Intro() {
+  const cl = `mt-24 text-6xl pb-2 pt-2 flex ${raleway.className}`;
   return (
-    <div className="mt-24 font-raleway text-6xl pb-2 pt-2 flex">
+    <div className={cl}>
       <Image
         className="rounded-full border-2 border-cyan-100 ml-8"
         src="/profile.jpeg"
